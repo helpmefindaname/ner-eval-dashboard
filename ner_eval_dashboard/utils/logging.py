d8d66c6979988ef5438959c0e50bee220d49198f
Benedikt Fuchs <benedikt.fuchs.staw@hotmail.com>
2022-09-18 01:30:09 +0200

    fix typing

--- ner_eval_dashboard/utils/logging.py
+++ ner_eval_dashboard/utils/logging.py
@@ -1,29 +1,31 @@
 import logging
 import sys
+from types import FrameType
+from typing import Optional, Union
 
-import flair.file_utils
 from loguru import logger
 
 
 class InterceptHandler(logging.Handler):
-    def emit(self, record):
+    def emit(self, record: logging.LogRecord) -> None:
         # Get corresponding Loguru level if it exists.
         try:
-            level = logger.level(record.levelname).name
+            level: Union[str, int] = logger.level(record.levelname).name
         except ValueError:
             level = record.levelno
 
         # Find caller from where originated the logged message.
-        frame, depth = sys._getframe(6), 6
-        while frame and frame.f_code.co_filename == logging.__file__:
+        frame: Optional[FrameType] = sys._getframe(6)
+        depth = 6
+        while frame is not None and frame.f_code.co_filename == logging.__file__:
             frame = frame.f_back
             depth += 1
 
         logger.opt(depth=depth, exception=record.exc_info).log(level, record.getMessage())
 
 
-def setup_logger():
-    flair_logger =  logging.getLogger("flair")
+def setup_logger() -> None:
+    flair_logger = logging.getLogger("flair")
     logging.basicConfig(handlers=[InterceptHandler()], force=True)
     for handler in flair_logger.handlers:
         logging.getLogger("flair").removeHandler(handler)
