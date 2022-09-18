be6ab8690d2d489567a8127578bc148dff219edc
Benedikt Fuchs <benedikt.fuchs.staw@hotmail.com>
2022-09-18 21:47:16 +0200

    add ScoredTokenPredictorMixin

--- ner_eval_dashboard/predictor/flair.py
+++ ner_eval_dashboard/predictor/flair.py
@@ -6,13 +6,22 @@ from flair.data import Sentence, Span
 from flair.data import Token as FlairToken
 from flair.models import SequenceTagger
 
-from ner_eval_dashboard.datamodels import Label, LabeledTokenizedText, PreTokenizedText
+from ner_eval_dashboard.datamodels import (
+    Label,
+    LabeledTokenizedText,
+    PreTokenizedText,
+    ScoredLabel,
+    ScoredTokenizedText,
+)
 from ner_eval_dashboard.predictor import Predictor
-from ner_eval_dashboard.predictor_mixins import DropoutPredictorMixin
+from ner_eval_dashboard.predictor_mixins import (
+    DropoutPredictorMixin,
+    ScoredTokenPredictorMixin,
+)
 
 
 @Predictor.register("FLAIR")
-class FlairPredictor(DropoutPredictorMixin, Predictor):
+class FlairPredictor(ScoredTokenPredictorMixin, DropoutPredictorMixin, Predictor):
     def __init__(self, name_or_path: str):
         super(FlairPredictor, self).__init__()
         with warnings.catch_warnings():
@@ -47,6 +56,10 @@ class FlairPredictor(DropoutPredictorMixin, Predictor):
 
         return self._label_names
 
+    @property
+    def token_label_names(self) -> List[str]:
+        return sorted(self.tagger.label_dictionary.get_items())
+
     def predict(self, data: List[PreTokenizedText]) -> List[LabeledTokenizedText]:
         sentences = list(map(self._tokenized_text_to_sentence, data))
         self.tagger.eval()
@@ -63,6 +76,14 @@ class FlairPredictor(DropoutPredictorMixin, Predictor):
         labels = map(self._sentence_to_labels, sentences)
         return self._combine_with_labels(data, labels)
 
+    def predict_token_scores(self, data: List[PreTokenizedText]) -> List[ScoredTokenizedText]:
+        sentences = list(map(self._tokenized_text_to_sentence, data))
+        self.tagger.eval()
+        self.tagger.predict(sentences)
+
+        scores = map(self._sentence_to_scores, sentences)
+        return self._combine_with_scores(data, scores)
+
     def _sentence_to_labels(self, sentence: Sentence) -> List[Label]:
         return [
             Label.construct(
@@ -72,6 +93,18 @@ class FlairPredictor(DropoutPredictorMixin, Predictor):
                 text=self._get_span_text(span),
             )
             for span in sentence.get_spans(self.tagger.label_type)
+        ]
+
+    def _sentence_to_scores(self, sentence: Sentence) -> List[List[ScoredLabel]]:
+        return [
+            [
+                ScoredLabel.construct(
+                    tag=label.value,
+                    score=label.score,
+                )
+                for label in token.get_tags_proba_dist(self.tagger.label_type)
+            ]
+            for token in sentence.tokens
         ]
 
     @staticmethod
--- ner_eval_dashboard/predictor_mixins.py
+++ ner_eval_dashboard/predictor_mixins.py
@@ -2,7 +2,11 @@ import abc
 import typing
 from typing import List, Type
 
-from ner_eval_dashboard.datamodels import LabeledTokenizedText, PreTokenizedText
+from ner_eval_dashboard.datamodels import (
+    LabeledTokenizedText,
+    PreTokenizedText,
+    ScoredTokenizedText,
+)
 
 if typing.TYPE_CHECKING:
     from ner_eval_dashboard.component import Component
@@ -25,4 +29,18 @@ class DropoutPredictorMixin(PredictorMixin):
 
     @abc.abstractmethod
     def predict_with_dropout(self, data: List[PreTokenizedText]) -> List[LabeledTokenizedText]:
+        raise NotImplementedError()
+
+
+class ScoredTokenPredictorMixin(PredictorMixin):
+    def __init__(self, *args: typing.Any, **kwargs: typing.Any) -> None:
+        super(ScoredTokenPredictorMixin, self).__init__(*args, **kwargs)
+
+    @property
+    @abc.abstractmethod
+    def token_label_names(self) -> List[str]:
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def predict_token_scores(self, data: List[PreTokenizedText]) -> List[ScoredTokenizedText]:
         raise NotImplementedError()
